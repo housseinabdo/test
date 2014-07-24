fe9b0faca966ae17a1a530609333f7b4c4f5768d
houssein <houssein@130.190.45.82>
2014-07-24 10:36:32 +0200

    initial check in

---
+++ h/src/h/aga.java
@@ -0,0 +1,10 @@
+package h;
+
+import fuzzydl.TriangularFuzzyNumber;
+import fuzzydl.exception.FuzzyOntologyException;
+
+public class aga {
+public static void main(String[] args) throws FuzzyOntologyException {
+	TriangularFuzzyNumber m = new TriangularFuzzyNumber(0,1,2);
+}
+}
